bfe397d1d41dc7675f8b32bfe01ed041828d6b08
Natalia Moskvychova <natashamoskv@gmail.com>
2023-07-18 22:36:37 +0300

    update styles for hero

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-
-const HULK_ID = '1009351';
-const SPIDER_MAN_ID = '1009610';
-const BLACK_PANTHER_ID = '1009187'; //id: 1011130, name: 'Black Panther (Ultimate)'
-const heroCharacters = [];
-
 import axios from 'axios';
 import md5 from 'md5';
 
+import img11 from './images/hero1.1_desktop.png'
+import img12 from './images/hero1.2_desktop.png'
+import img21 from './images/hero2.1_desktop.png'
+import img22 from './images/hero2.2_desktop.png'
+import img31 from './images/hero3.1_desktop.png'
+import img32 from './images/hero3.2_desktop.png'
 
-const heroSwiperWrapper = document.querySelector('.swiper-wrapper');
+const HULK_ID = '1009351';
+const SPIDER_MAN_ID = '1009610';
+const BLACK_PANTHER_ID = '1009187';
 
-const PUBLIC_KEY = '3b19de822bbdb85fd464fb1da19ae275'; // Ваш публичный ключ Marvel API
-const PRIVATE_KEY = 'f26e701f59fc8c3169b6ee92603774e03d9615c5'; // Ваш приватный ключ Marvel API
+const PUBLIC_KEY = '3b19de822bbdb85fd464fb1da19ae275'; 
+const PRIVATE_KEY = 'f26e701f59fc8c3169b6ee92603774e03d9615c5';
+const BASE_URL = 'https://gateway.marvel.com/v1/public/characters';
 
 const timestamp = new Date().getTime().toString();
-const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY); // Вычисляем хэш
-
-const BASE_URL = 'https://gateway.marvel.com/v1/public/characters';
+const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
 
 function fetchCharacterById(characterId) {
     return axios.get(`${BASE_URL}/${characterId}?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`)
         .then((response) => {
-            const res = response.data.data.results[0]
-            if (res.name === 'Black Panther') res.description = 'T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.'
-            return res;
+            return response.data.data.results[0];
         });
 }
 
-const heroCharacterIds = [BLACK_PANTHER_ID, SPIDER_MAN_ID, HULK_ID, ];
+const heroCharacterIds = [BLACK_PANTHER_ID, SPIDER_MAN_ID, HULK_ID ];
 
 Promise.all(heroCharacterIds.map(fetchCharacterById))
   .then((heroCharacters) => {
@@ -38,37 +37,53 @@ Promise.all(heroCharacterIds.map(fetchCharacterById))
     console.error(error);
   });
 
+
 function createHeroSwiperMarkup(heroCharacters) {
     heroCharacters.forEach((char) => {
-        if (char.name === 'Spider-Man (Peter Parker)') char.name = 'Spider Man';
         let swiperSlide;
-        // const markup = `<div class="swiper-effect">
-        //   <p class="swiper-text">${char.name}</p>
-        //     </div>
-        //     `;
-        const markup = `<div class="swiper-effect">
-          <img src="${char.thumbnail.path}/portrait_xlarge.${char.thumbnail.extension}" alt="${char.name}" width=220>
-                <p class="swiper-text">${char.name}</p>
-            </div>
-            `;
-        const heroButton = document.querySelector('.hero-button');
-        
+
         switch (char.name) {
             case 'Black Panther':
+                char.image1 = img11;
+                char.image2 = img12;
+                char.description = 'T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther.'
+                
                 swiperSlide = document.querySelector('[aria-label="1 / 3"]');
-                swiperSlide.innerHTML = markup;
                 break;
-            case 'Spider Man':
+            case 'Spider-Man (Peter Parker)':
+                char.name = 'Spider Man';
+                char.image1 = img21;
+                char.image2 = img22;
+                char.description = 'With amazing spider-like abilities, teenage science whiz Peter Parker fights crime and dreams of becoming an Avenger as Spider-Man.'
                 swiperSlide = document.querySelector('[aria-label="2 / 3"]');
-                swiperSlide.innerHTML = markup;
+                
                 break;
             case 'Hulk':
+                char.image1 = img31;
+                char.image2 = img32;
+                
                 swiperSlide = document.querySelector('[aria-label="3 / 3"]');
-                swiperSlide.innerHTML = markup;
+                char.description = 'Exposed to heavy doses of gamma radiation, scientist Bruce Banner transforms into the mean, green rage machine called the Hulk.'
                 break;
         }
-
-       
         
+        const markup = `
+         <div class="hero-swiper__container hero-swiper__first-container">
+              <img class="hero-swiper__img1" src="${char.image1}" alt="${char.name}" width=352 height=540>
+            </div>
+            <div class="hero-swiper__container hero-swiper__second-container">
+              <img class="hero-swiper__img2 " src="${char.image2}" alt="${char.name}" width=352 height=540>
+              <div class="hero-swiper__text-effect">
+                <p class="hero-swiper__text">${char.name}</p>
+              </div>
+             
+              <div class="hero-swiper__description-container">
+                <p class="hero-swiper__description-title">Characters</p>
+                <p class="hero-swiper__description-text">${char.description}</p>
+              </div>
+            </div>
+            `;
+        
+        swiperSlide.innerHTML = markup;
     });
   }
